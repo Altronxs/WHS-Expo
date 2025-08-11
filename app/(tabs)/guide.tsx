3cb21ffd2b64a8d02aef2462d70f54c8cb5c4a9c
Altronxs <kylextron101098@gmail.com>
2025-08-11 00:12:43 -1000

    home

---
+++ app/(tabs)/guide.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+const Guide = () => {
+    return (
+        <View>
+            <Text>Guide</Text>
+        </View>
+    )
+}
+
+export default Guide
+const styles = StyleSheet.create({})
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,51 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Text, Button, Alert, Image, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Link, useRouter } from "expo-router";
+import { useFonts } from '@expo-google-fonts/roboto/useFonts';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function Index() {
+  const router = useRouter(); // Get the router instance
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
 
-export default function HomeScreen() {
+  if (!fontsLoaded) {
+    return null; // Or a loading indicator
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
+    
+    <SafeAreaProvider className="flex-col">
+      {/* <SafeAreaView className="flex-none justify-start items-start bg-blue-950">
+        <TouchableOpacity 
+          onPress={() => router.push("/map")}
+          className="flex-row items-center"
+        >
+          <Image
+            source={require("@/assets/images/menu.png")}
+            className="size-10 relative mt-5 mb-2 left-4"
+          />
+        </TouchableOpacity>
+      </SafeAreaView> */}
+      <SafeAreaView className="flex-row bg-blue-950 h-28 z-10 pt-28">
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require("@/assets/images/whs-logo.png")}
+          className="w-32 h-32 relative bottom-28 left-11"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <SafeAreaView className=" w-48 h-28 bottom-36 left-14 items-start">
+          <Text className="text-white font-roboto-bold">MY VOICE</Text>
+          <Text className="text-white font-roboto-bold">     MY CHOICE</Text>
+          <Text className="text-white font-roboto-bold">           MY FUTURE</Text>
+        </SafeAreaView>
+
+      </SafeAreaView>
+      <SafeAreaView className="grow  justify-center items-center bg-white">
+        <Text className="text-5xl text-blue-500 font-bold font-roboto-italic ">WAIPAHU</Text>
+        <Link href="/map">map</Link>
+      </SafeAreaView>
+    </SafeAreaProvider>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
